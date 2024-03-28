76e6401daa1f59528f03ed2201769c65ae433bfb
serenaczhu <159599404+serenaczhu@users.noreply.github.com>
2024-03-28 13:11:23 -0400

    Update ArrayList.java

--- HW3_Serena_Zhu/ArrayList.java
+++ HW3_Serena_Zhu/ArrayList.java
@@ -1,16 +1,17 @@
 public class ArrayList {
     
-    int[] a;
-    int size;
-    MAX_CAPACITY = 10000;
+    private int[] list;
+    private int size;
+    private final int DEFAULT_CAPACITY = 1;
 
     public ArrayList() {
-        a = new int[MAX_CAPACITY];
+        list = new int[DEFAULT_CAPACITY];
         size = 0;
     }
 
     public void add(int n) {
-        a[size] = n;
+        ensureCapacity();
+        list[size] = n;
         size++;
     }
 
@@ -18,45 +19,108 @@ public class ArrayList {
         if(index < 0) {
             throw new IllegalArgumentException();
         }
+        ensureCapacity();
         if(index > size) {
-            a[size] = n;
+            list[size] = n;
             size++;
         }
         else {
-            for()
+            for(int i = size; i >= index; i--) {
+                list[i] = list[i - 1];
+            }
+            list[index] = n;
+            size++;
         }
     }
 
     public int indexof(int n) {
-
+        int index = -1;
+        for(int i = 0; i < list.length; i++) {
+            if(list[i] == n) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
     public int remove(int index) {
-
+        if(index < 0) {
+            throw new IllegalArgumentException();
+        }
+        if(index > size - 1) {
+            int save = list[size - 1];
+            list[size - 1] = 0;
+            return save;
+        }
+        else {
+            int save = list[index];
+            for(int i = index; i < size - 1; i++) {
+                list[i] = list[i + 1];
+            }
+            return save;
+        }
     }
 
     public void removeValue(int n) {
-
+        for(int i = indexof(n); i < size - 1; i++) {
+            list[i] = list[i + 1];
+        }
     }
 
     public void removeall(int n) {
-
+        for(int number : list) {
+            if(number == n) {
+                removeValue(n);
+            }
+        }
     }
 
     public double mean() {
-
+        double sum = 0;
+        for(int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        return sum /= size;
     }
 
     public double variance() {
-
+        double summation = 0;
+        for(int number : list) {
+            summation = (number - this.mean()) * (number - this.mean());
+        }
+        return summation /= (size - 1);
     }
 
     public ArrayList sublist(int lower, int upper) {
-
+        int[] temp = new int[list.length];
+        int count = 0;
+        for(int i = lower; i <= upper; i++) {
+            temp[count] = list[i];
+            count++;
+        }
+        list = temp;
+        return this;
     }
 
     public ArrayList removeNoise() {
+        double stanDev = Math.sqrt(this.variance());
+        for(int number : list) {
+            if(number > (this.mean() + (3 * stanDev)) || number < (this.mean() - (3 * stanDev))) {
+                this.remove(number);
+            }
+        }
+        return this;
+    }
 
+    private void ensureCapacity() {
+        if((size + 1) == list.length) {
+            int[] temp = new int[2 * list.length];
+            for(int i = 0; i < size; i++) {
+                temp[i] = list[i];
+            }
+            list = temp;
+        }
     }
 
 }
