dd0101328b3d9984e5de4da78ec7f3217ed178ab
serenaczhu <159599404+serenaczhu@users.noreply.github.com>
2024-04-15 23:49:55 -0400

    Update RandomQuickSort.java

--- HW4/HW4_Serena_Zhu/RandomQuickSort.java
+++ HW4/HW4_Serena_Zhu/RandomQuickSort.java
@@ -17,19 +17,22 @@ public class RandomQuickSort {
         }
         else {
             int pivotIndex = partition(nums, first, last);
-            quickSort(nums, first, pivotIndex - 1);
-            quickSort(nums, pivotIndex + 1, last);
-            //int pivotIndex = partition(nums, first, last);
-            //quickSort(nums, first, pivotIndex - 1);
-            //quickSort(nums, pivotIndex + 1, last);
+            if(pivotIndex - 1 > first) {
+                quickSort(nums, first, pivotIndex - 1);
+            }
+            if(pivotIndex + 1 < last) {
+                quickSort(nums, pivotIndex + 1, last);
+            }
         }
     }
 
     private static <T extends Comparable <? super T>> int partition(T[] nums, int first, int last) {
         // chooses random pivot
-        int pivotIndex = rng.nextInt(last - first + 1);
+        int pivotIndex = first + rng.nextInt(last - first + 1);
+        System.out.println("index: " + pivotIndex);
         //int pivotIndex = rng.nextInt(nums.length - 1);
         T pivotValue = nums[pivotIndex];
+        System.out.println("value: " + pivotValue);
 
         // exchange pivot element with last element
         T save = nums[last];
@@ -39,25 +42,26 @@ public class RandomQuickSort {
 
         int indexFromLeft = first;
         int indexFromRight = last - 1;
+        System.out.println("iFL: " + indexFromLeft + "iFR: " + indexFromRight);
         boolean done = false;
 
         while(!done) {
-            while(nums[indexFromLeft].compareTo(pivotValue) < 0) {
+            while(nums[indexFromLeft].compareTo(pivotValue) < 0 && indexFromLeft < pivotIndex) {
                 indexFromLeft++;
-                System.out.println("iFL: " + indexFromLeft);
+                System.out.println("iFL: " + indexFromLeft + "iFR: " + indexFromRight);
             }
-            while(nums[indexFromRight].compareTo(pivotValue) > 0) {
+            while(nums[indexFromRight].compareTo(pivotValue) > 0 && indexFromRight > 0) {
                 indexFromRight--;
-                System.out.println("iFR: " + indexFromRight);
+                System.out.println("iFL: " + indexFromLeft + "iFR: " + indexFromRight);
             }
             if(indexFromLeft < indexFromRight) {
                 T leftValue = nums[indexFromLeft];
                 nums[indexFromLeft] = nums[indexFromRight];
                 nums[indexFromRight] = leftValue;
                 indexFromLeft++;
-                System.out.println("iFL: " + indexFromLeft);
+                System.out.println("iFL: " + indexFromLeft + "iFR: " + indexFromRight);
                 indexFromRight--;
-                System.out.println("iFR: " + indexFromRight);
+                System.out.println("iFL: " + indexFromLeft + "iFR: " + indexFromRight);
             }
             else {
                 done = true;
@@ -71,39 +75,5 @@ public class RandomQuickSort {
 
         return pivotIndex;
     }
-
-    /*
-    private static <T extends Comparable<? super T>> void insertionSort(T[] nums, int first, int last) {
-        for (int i = first + 1; i <= last; i++) {
-            T key = nums[i];
-            int j = i - 1;
-    
-            while (j >= first && nums[j].compareTo(key) > 0) {
-                nums[j + 1] = nums[j];
-                j = j - 1;
-            }
-            nums[j + 1] = key;
-        }
-    }
-    */
-
-    /*
-    private static <T extends Comparable<? super T>> void insertionSort(T[] nums, int first, int last) {
-        if(first < last) {
-            insertionSort(nums, first, last - 1);
-            insertInOrder(nums[last], nums, first, last - 1);
-        }
-    }
-
-    private static <T extends Comparable<? super T>> void insertInOrder(T entry, T[] nums, int begin, int end) {
-        if(entry.compareTo(nums[end]) > 0) {
-            nums[end + 1] = entry;
-        }
-        else {
-            nums[end + 1] = nums[end];
-            insertInOrder(entry, nums, begin, end - 1);
-        }
-    }
-    */
     
 }
