13121b925f0a3a58b952bb1d4db2031de4cef3fb
serenaczhu <159599404+serenaczhu@users.noreply.github.com>
2024-04-15 23:59:09 -0400

    Update RandomQuickSortTester.java

--- HW4/HW4_Serena_Zhu/RandomQuickSortTester.java
+++ HW4/HW4_Serena_Zhu/RandomQuickSortTester.java
@@ -5,10 +5,22 @@ public class RandomQuickSortTester {
     
     @Test
     public void testQuickSort() {
-        Integer[] intArr = new Integer[] {3, 5, 3, 2, 1};
-        Integer[] expIntArr = new Integer[] {1, 2, 3, 3, 5};
-        RandomQuickSort.quickSort(intArr, 0, intArr.length - 1);
-        assertTrue(intArr.equals(expIntArr));
+        // tests an unordered array
+        Integer[] a1 = new Integer[] {3, 5, 3, 2, 1};
+        Integer[] expected1 = new Integer[] {1, 2, 3, 3, 5};
+        RandomQuickSort.quickSort(a1, 0, a1.length - 1);
+        assertArrayEquals(expected1, a1);
+
+        // tests a longer unordered array
+        Integer[] a3 = new Integer[] {9, 8, 8, 5, 7, 7, 4, 4, 4, 2};
+        Integer[] expected3 = new Integer[] {2, 4, 4, 4, 5, 7, 7, 8, 8, 9};
+        RandomQuickSort.quickSort(a3, 0, a3.length - 1);
+        assertArrayEquals(expected3, a3);
+
+        // tests a sorted array
+        Integer[] a2 = new Integer[] {1, 2, 3, 3, 5};
+        RandomQuickSort.quickSort(a2, 0, a2.length - 1);
+        assertArrayEquals(expected1, a2);
     }
 
 }
