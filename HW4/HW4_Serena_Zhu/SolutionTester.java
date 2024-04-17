c206ad4a2ece5be4b58abe648241e08dc4789fb4
serenaczhu <159599404+serenaczhu@users.noreply.github.com>
2024-04-17 00:44:28 -0400

    Update SolutionTester.java

--- HW4/HW4_Serena_Zhu/SolutionTester.java
+++ HW4/HW4_Serena_Zhu/SolutionTester.java
@@ -14,6 +14,14 @@ public class SolutionTester {
         int[][] a2 = new int[][] {{0, 0}, {1, 1}, {200, 200}, {200, 233}, {300, 300}};
         int[][] expect2 = new int[][] {{200, 233}, {200, 200}};
         assertArrayEquals(expect2, s.kClosest(a2, 2));
+
+        int[][] a3 = new int[][] {{0, 0}, {1, 1}, {200, 200}, {200, 233}, {300, 300}};
+        int[][] expect3 = new int[][] {{200, 233}, {200, 200}, {300, 300}};
+        assertArrayEquals(expect3, s.kClosest(a3, 3));
+        
+        int[][] a4 = new int[][] {{0, 0}, {1, 1}, {200, 200}, {200, 233}, {233, 200}, {300, 300}};
+        int[][] expect4 = new int[][] {{200, 233}, {233, 200}};
+        assertArrayEquals(expect4, s.kClosest(a4, 2));
     }
 
 }
